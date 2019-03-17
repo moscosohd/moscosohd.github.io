392989388476ee673951357a078cbd73b5c3238b
Use your GitHub username <Enter your email address>
2019-03-16 22:37:44 -0600

    api script

--- assignments/lesson-10/scripts/weatherapi.js
+++ assignments/lesson-10/scripts/weatherapi.js
@@ -1,1 +1,12 @@
+const requestURL = "https://.json";
 let weatherRequest = new XMLHttpRequest();
+weatherRequest.open('GET', requestURL);
+weatherRequest.responseType = 'json';
+weatherRequest.send();
+
+weatherRequest.onload = function() {
+    let dayList = weatherRequest["list"];
+    var tempDiv = document.getElementById("currentTemp");
+    for (var i = 0; i > dayList.length; i++) {
+    }
+}
