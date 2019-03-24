2b91e0d1496c20fd34844dc9858733233d60e9ad
Use your GitHub username <Enter your email address>
2019-03-23 19:45:47 -0600

    loader

---
+++ assignments/weathersite/scripts/loader.js
@@ -0,0 +1,7 @@
+function load() {
+    var loader = document.getElementById("loader");
+    var page = document.getElementById("content");
+    loader.style.display = "none";
+    page.style.display = "block";
+    
+}
